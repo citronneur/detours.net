4df8c135a2965b27261e5590b9df584bb4920c45
Sylvain Peyrefitte <citronneur@gmail.com>
2018-10-06 22:26:33 +0200

    Initial commit build system is available

---
+++ detours.net/inject/src/dninject.cpp
@@ -0,0 +1,6 @@
+#include <Windows.h>
+#include "detours.h"
+
+BOOL WINAPI DllMain(_In_ HINSTANCE hinstDLL, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved) {
+	  
+}
---
+++ detours.net/loader/src/DetoursNet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (FileStream fs = File.Create("c:\\dev\\foo"))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+        }
+    }
+}
