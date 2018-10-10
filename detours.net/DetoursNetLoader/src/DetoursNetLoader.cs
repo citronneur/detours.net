16593f48a025808f029f7afa9cbbb4ac7712732f
Sylvain Peyrefitte <citronneur@gmail.com>
2018-10-10 22:09:41 +0200

    First hook crash...

--- detours.net/DetoursNetLoader/src/DetoursNetLoader.cs
+++ detours.net/DetoursNetLoader/src/DetoursNetLoader.cs
@@ -6,17 +6,46 @@ namespace detoursnetloader
 {
     public class DetoursNetLoader
     {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetCurrentThread();
+
+        [DllImport("kernel32.dll", EntryPoint = "GetModuleHandleA", CharSet = CharSet.Ansi)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("Detours.dll")]
+        internal static extern long DetourAttach(ref IntPtr a, IntPtr b);
+
+        [DllImport("Detours.dll")]
+        internal static extern long DetourUpdateThread(IntPtr a);
+
+        [DllImport("Detours.dll")]
+        internal static extern long DetourTransactionBegin();
+
+        [DllImport("Detours.dll")]
+        internal static extern long DetourTransactionCommit();
+
+        delegate int GetCurrentProcessIdDelegate();
         /// <summary>
         /// Main entry point of loader
         /// </summary>
         public static void DetoursNetLoader_Start()
         {
-            //System.Diagnostics.Debugger.Launch();
-            // Get back the env variables
+
             Assembly plugin = Assembly.LoadFile("c:\\dev\\build_x64\\bin\\Debug\\TraceCreateFile.dll");
-            MethodInfo method = plugin.GetType("tracecreatefile.TraceCreateFile").GetMethod("GetModuleHandleHook");
-            Delegate methodDelegate = Delegate.CreateDelegate(method.GetType(), method);
-            Marshal.GetFunctionPointerForDelegate(methodDelegate);
+            MethodInfo method = plugin.GetType("tracecreatefile.TraceCreateFile").GetMethod("MyGetCurrentProcessId");
+            
+            Delegate methodDelegate = Delegate.CreateDelegate(typeof(GetCurrentProcessIdDelegate), method);
+            
+            IntPtr a = Marshal.GetFunctionPointerForDelegate(methodDelegate);
+            IntPtr kernel32 = GetModuleHandle("kernel32");
+            IntPtr pGetCurrentProcessId = GetProcAddress(kernel32, "GetCurrentProcessId");
+            DetourTransactionBegin();
+            DetourUpdateThread(GetCurrentThread());
+            DetourAttach(ref pGetCurrentProcessId, a);
+            long ret =  DetourTransactionCommit();
         }
     }
 }
