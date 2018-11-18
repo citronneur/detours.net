68ddaed9e1647309fcffae0b7500471260d3cbca
Sylvain Peyrefitte <citronneur@gmail.com>
2018-11-18 10:25:57 +0100

    Fix log

--- plugins/procmon/src/Procmon.cs
+++ plugins/procmon/src/Procmon.cs
@@ -3,10 +3,28 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using DetoursNet;
 
-namespace ftrace
+namespace procmon
 {
     public static class Kernel32
     {
+        /// <summary>
+        /// Just for sync console
+        /// </summary>
+        private static object sSync = new object();
+
+        /// <summary>
+        /// Write message to console in selected color
+        /// </summary>
+        /// <param name="message">message to write</param>
+        /// <param name="color">output color</param>
+        private static void WriteColor(ConsoleColor color, string message)
+        {
+            ConsoleColor old = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ForegroundColor = old;
+        }
+
         [DllImport("kernel32.dll")]
         private extern static bool AllocConsole();
 
@@ -30,8 +48,8 @@ namespace ftrace
         /// <param name="dwFlagsAndAttributes"></param>
         /// <param name="hTemplateFile"></param>
         /// <returns></returns>
-        public delegate IntPtr CreateFileDelegate(
-            IntPtr lpFileName,
+        public delegate IntPtr CreateFileWDelegate(
+            [MarshalAs(UnmanagedType.LPWStr)] string lpFileName,
             uint dwDesiredAccess,
             uint dwShareMode,
             IntPtr SecurityAttributes,
@@ -51,53 +69,101 @@ namespace ftrace
         /// <param name="dwFlagsAndAttributes"></param>
         /// <param name="hTemplateFile"></param>
         /// <returns></returns>
-        [Detours("kernel32.dll", typeof(CreateFileDelegate))]
+        [Detours("kernel32.dll", typeof(CreateFileWDelegate))]
         public static IntPtr CreateFileW(
-            IntPtr lpFileName,
+            string lpFileName,
             uint dwDesiredAccess,
             uint dwShareMode,
             IntPtr SecurityAttributes,
             uint dwCreationDisposition,
             uint dwFlagsAndAttributes,
             IntPtr hTemplateFile
-        ) {
-            
-            
-            IntPtr result = ((CreateFileDelegate)DelegateStore.GetReal(MethodInfo.GetCurrentMethod()))(lpFileName, dwDesiredAccess, dwShareMode, SecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+        )
+        {
+            IntPtr result = ((CreateFileWDelegate)DelegateStore.GetReal(MethodInfo.GetCurrentMethod()))(lpFileName, dwDesiredAccess, dwShareMode, SecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
 
-            Console.Write("CreateFile " + Marshal.PtrToStringUni(lpFileName) + " ");
-            if((int)result == -1)
+            lock(sSync)
             {
-                Console.WriteLine("FAILED");
-            }
-            else
-            {
-                Console.WriteLine("SUCCESS");
-            }
+                WriteColor(ConsoleColor.Yellow, "CreateFile");
+                Console.Write(" {" + lpFileName + "} ");
 
+                if ((int)result == -1)
+                {
+                    WriteColor(ConsoleColor.Red, "FAILED");
+                }
+                else
+                {
+                    WriteColor(ConsoleColor.Green, "SUCCESS");
+                }
+
+                Console.WriteLine();
+            }
+            
             return result;
         }
 
-        public delegate int RegOpenKeyExDelegate(IntPtr hKey, IntPtr lpSubKey, int ulOptions, int samDesired, IntPtr phkResult);
+        /// <summary>
+        /// Open a registry key delegate
+        /// </summary>
+        /// <param name="hKey"></param>
+        /// <param name="lpSubKey"></param>
+        /// <param name="ulOptions"></param>
+        /// <param name="samDesired"></param>
+        /// <param name="phkResult"></param>
+        /// <returns></returns>
+        public delegate int RegOpenKeyExWDelegate(
+            IntPtr hKey, 
+            [MarshalAs(UnmanagedType.LPWStr)] string lpSubKey, 
+            int ulOptions, 
+            int samDesired, 
+            IntPtr phkResult
+        );
 
-        [Detours("advapi32.dll", typeof(RegOpenKeyExDelegate))]
-        public static int RegOpenKeyExW(IntPtr hKey, IntPtr lpSubKey, int ulOptions, int samDesired, IntPtr phkResult)
+        /// <summary>
+        /// Hook of open registry key function
+        /// </summary>
+        /// <param name="hKey"></param>
+        /// <param name="lpSubKey"></param>
+        /// <param name="ulOptions"></param>
+        /// <param name="samDesired"></param>
+        /// <param name="phkResult"></param>
+        /// <returns></returns>
+        [Detours("advapi32.dll", typeof(RegOpenKeyExWDelegate))]
+        public static int RegOpenKeyExW(
+            IntPtr hKey, 
+            string lpSubKey, 
+            int ulOptions, 
+            int samDesired, 
+            IntPtr phkResult
+        )
         {
-            int result = ((RegOpenKeyExDelegate)DelegateStore.GetReal(MethodInfo.GetCurrentMethod()))(hKey, lpSubKey, ulOptions, samDesired, phkResult);
-            Console.Write("RegOpenKey " + Marshal.PtrToStringUni(lpSubKey) + " ");
+            int result = ((RegOpenKeyExWDelegate)DelegateStore.GetReal(MethodInfo.GetCurrentMethod()))(hKey, lpSubKey, ulOptions, samDesired, phkResult);
 
-            switch(result)
+            if(lpSubKey != null && lpSubKey.Length > 0)
             {
-                case 0:
-                    Console.WriteLine("SUCCESS");
-                    break;
-                case 5:
-                    Console.WriteLine("ACCESS DENIED");
-                    break;
-                default:
-                    Console.WriteLine("UNKNOWN");
-                    break;
+                lock(sSync)
+                {
+                    WriteColor(ConsoleColor.Yellow, "RegOpenKey");
+
+                    Console.Write(" {" + lpSubKey + "} ");
+
+                    switch (result)
+                    {
+                        case 0:
+                            WriteColor(ConsoleColor.Green, "SUCCESS");
+                            break;
+                        case 5:
+                            WriteColor(ConsoleColor.Red, "ACCESS DENIED");
+                            break;
+                        default:
+                            WriteColor(ConsoleColor.Red, "UNKNOWN (" + result + ")");
+                            break;
+                    }
+
+                    Console.WriteLine();
+                }
             }
+            
             return result;
         }
     }
