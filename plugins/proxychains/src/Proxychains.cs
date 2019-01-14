8ae4f4dca932bc5c96517fc607cc5566263f725e
citronneur <citronneur@gmail.com>
2019-01-14 21:46:33 +0100

    Add SOCKS5 support for proxychains plugin

--- plugins/proxychains/src/Proxychains.cs
+++ plugins/proxychains/src/Proxychains.cs
@@ -2,32 +2,12 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 using DetoursNet;
+using proxychains;
 
 namespace Proxychains
 {
-    public class WS2_32
+    public class Hooks
     {
-        [StructLayout(LayoutKind.Sequential)]
-        public struct in_addr
-        {
-            public byte s_b1;
-            public byte s_b2;
-            public byte s_b3;
-            public byte s_b4;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct sockaddr_in
-        {
-            public short sin_family;
-            public ushort sin_port;
-            public in_addr sin_addr;
-            private Int64 Zero;
-        }
-
-        [DllImport("ws2_32.dll")]
-        public static extern int send(IntPtr Socket, IntPtr buff, int len, int flags);
-
         /// <summary>
         /// Delegate of connect function from WS2_32.dll
         /// </summary>
@@ -35,7 +15,7 @@ namespace Proxychains
         /// <param name="name">struct dest connection infos</param>
         /// <param name="namelen">sizeof name</param>
         /// <returns></returns>
-        public delegate int ConnectDelegate(IntPtr s, ref sockaddr_in name, int namelen);
+        public delegate int ConnectDelegate(IntPtr s, ref Socket.sockaddr_in name, int namelen);
 
         /// <summary>
         /// Hook of native connect function from WS2_32.dll
@@ -45,21 +25,21 @@ namespace Proxychains
         /// <param name="namelen">sizeof name</param>
         /// <returns></returns>
         [Detours("WS2_32.dll", typeof(ConnectDelegate))]
-        public static int connect(IntPtr s, ref sockaddr_in name, int namelen)
+        public static int connect(IntPtr s, ref Socket.sockaddr_in name, int namelen)
         {
             Console.WriteLine("connect hooked !!!! family: " + name.sin_addr.s_b1 + "." + name.sin_addr.s_b2 + "." + name.sin_addr.s_b3 + "." + name.sin_addr.s_b4 + " port:" + (UInt16)(name.sin_port << 8 | name.sin_port >> 8));
             var connectCallback = (ConnectDelegate)(DelegateStore.GetReal(MethodInfo.GetCurrentMethod()));
 
-            var socks = new sockaddr_in()
+            var socks = new Socket.sockaddr_in()
             {
-                sin_addr = new in_addr()
+                sin_addr = new Socket.in_addr()
                 {
-                    s_b1 = 192,
-                    s_b2 = 168,
+                    s_b1 = 127,
+                    s_b2 = 0,
                     s_b3 = 0,
-                    s_b4 = 11
+                    s_b4 = 1
                 },
-                sin_port = (UInt16)((9050 << 8 & 0xFF00) | (9050 >> 8 & 0xFF)),
+                sin_port = Socket.Hston(9090),
                 sin_family = name.sin_family
             };
 
@@ -69,7 +49,16 @@ namespace Proxychains
                 return result;
             }
 
-            //send(s, )
+            // Authenticate client  
+            try
+            {
+                Socks5.Connect(s, name.sin_addr, Socket.Ntohs(name.sin_port));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error during SOCKS connection : " + e);
+                return -1;
+            }
 
             return 0;
         }
---
+++ plugins/proxychains/src/Socks5.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace proxychains
+{
+    /// <summary>
+    /// Implement SOCKS5 client protocol
+    /// </summary>
+    public class Socks5
+    {
+        /// <summary>
+        /// Basic SOCKS5 authenticate with None authentication
+        /// </summary>
+        /// <param name="socket">Socket use for socks</param>
+        static void Authenticate(IntPtr socket)
+        {
+            Socket.Send(socket, new byte[]
+            {
+                0x05,
+                0x01,
+                0x00    // none auth method
+            });
+
+            var response = Socket.Recv(socket, 2);
+
+            if(response[0] != 0x05)
+            {
+                throw new Exception("Invalid SOCKS version");
+            }
+
+            if(response[1] != 0x00)
+            {
+                throw new Exception("Invalid Auth method selected");
+            }
+        }
+
+        /// <summary>
+        /// Create a connection with a SOCKS server
+        /// </summary>
+        /// <param name="socket">socket</param>
+        /// <param name="ip">dest ip over SOCKS</param>
+        /// <param name="port">dest port over SOCKS</param>
+        public static void Connect(IntPtr socket, Socket.in_addr ip, UInt16 port)
+        {
+            Authenticate(socket);
+
+            Socket.Send(socket, new byte[]
+            {
+                0x05,
+                0x01,   // TCP connection
+                0X00,
+                0x01,   // IPv4
+                ip.s_b1,
+                ip.s_b2,
+                ip.s_b3,
+                ip.s_b4,
+                (byte)(port >> 8 & 0xFF),
+                (byte)(port & 0xFF)
+            });
+
+            // Always wait for 10 bytes
+            var response = Socket.Recv(socket, 10);
+
+            if(response[0] != 0x05)
+            {
+                throw new Exception("Invalid version protocol");
+            }
+
+            // All other code is error code
+            if(response[1] != 0)
+            {
+                throw new Exception("SOCKS : server error : " + response[1]);
+            }
+        }
+    }
+}
